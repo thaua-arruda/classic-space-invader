fb48d38e2868579aa9ef327c14099ecd54684174
thaua-arruda <thauafarruda@gmail.com>
2024-05-15 16:24:01 -0400

    Draw method that draws the bullet on the canvas

--- src/javascript/Bullet.js
+++ src/javascript/Bullet.js
@@ -8,7 +8,10 @@ class Bullet {
 
         this.width = 5;
         this.height = 20;
-        
-
+    }
+    draw(ctx) {
+       this.y -= this.velocity
+       ctx.fillStyle = this.bulletColor;
+       ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 }
