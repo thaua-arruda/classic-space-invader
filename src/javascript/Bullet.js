1ccf1c5498093409f004b50689a524ae1824bf4e
thaua-arruda <thauafarruda@gmail.com>
2024-05-15 16:15:18 -0400

    added bullet class

---
+++ src/javascript/Bullet.js
@@ -0,0 +1,14 @@
+class Bullet {
+    constructor(canvas, x, y, velocity, bulletColor) {
+        this.canvas = canvas;
+        this.x = x;
+        this.y = y;
+        this.velocity = velocity;
+        this.bulletColor = bulletColor;
+
+        this.width = 5;
+        this.height = 20;
+        
+
+    }
+}
