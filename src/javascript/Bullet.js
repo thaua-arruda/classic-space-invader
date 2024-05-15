2dea81bd13fac068c4901dc5a5e6af4be991e826
thaua-arruda <thauafarruda@gmail.com>
2024-05-15 16:38:27 -0400

    collideWith method that checks whether a collision occurred

--- src/javascript/Bullet.js
+++ src/javascript/Bullet.js
@@ -14,4 +14,13 @@ class Bullet {
        ctx.fillStyle = this.bulletColor;
        ctx.fillRect(this.x, this.y, this.width, this.height);
     }
+    collideWith(sprite) {
+    if (this.x + this.width > sprite.x &&
+        this.x < sprite.x > sprite.width &&
+        this.y + this.height > sprite.y &&
+        this.y < sprite.y + sprite.height) {
+            return true;
+    } else {
+        return false;
+    }
 }
