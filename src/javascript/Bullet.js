39a68069d0346809f74c5f544490594fa91656e9
thaua-arruda <thauafarruda@gmail.com>
2024-05-20 15:56:28 -0400

    add shoot function in bulletController class

--- src/javascript/Bullet.js
+++ src/javascript/Bullet.js
@@ -1,4 +1,4 @@
-class Bullet {
+export default class Bullet {
     constructor(canvas, x, y, velocity, bulletColor) {
         this.canvas = canvas;
         this.x = x;
--- src/javascript/BulletController.js
+++ src/javascript/BulletController.js
@@ -1,4 +1,6 @@
-class BulletController {
+import Bullet from "./Bullet.js";
+
+export default class BulletController {
     bullets = [];
     timeTillNextBulletAllowed = 0;
 
@@ -32,4 +34,16 @@ class BulletController {
          } 
       return false;
     }
- }
+    shoot(x, y, velocity, timeTillNextBulletAllowed = 0) {
+        if (this.timeTillNextBulletAllowed <= 0 && this.bullets.length < this.maxBulletAtATime){
+            const bullet = new Bullet(this.canvas, x, y, velocity, this.bulletColor);
+            this.bullets.push(bullet);
+            if(this.soundEnable) {
+                this.shootSound.currentTime = 0;
+                this.shootSound.play();
+            }
+            this.timeTillNextBulletAllowed = timeTillNextBulletAllowed;
+        }
+    }
+}
+
