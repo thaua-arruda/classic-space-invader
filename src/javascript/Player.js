d2acde42092fe5c986c39d8561fb653e12e6b47b
thaua-arruda <thauafarruda@gmail.com>
2024-05-20 16:56:02 -0400

    add player class

---
+++ src/javascript/Player.js
@@ -0,0 +1,18 @@
+export default class Player{
+    rightPressed = false;
+    leftPressed = false;
+    shootPressed = false;
+
+    constructor(canvas, velocity, bulletController) {
+        this.canvas = canvas;
+        this.velocity = velocity;
+        this.bulletController = bulletController;
+        this.x = this.canvas.width / 2;
+        this.y = this.canvas.height - 75;
+        this.width = 50;
+        this.height = 48;
+        this.image = new Image();
+        this.image.src = "src/assets/images/player.png";
+
+ }
+}
