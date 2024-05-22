c1dca8683a15270d02a58658931e42410ed6570b
thaua-arruda <thauafarruda@gmail.com>
2024-05-22 17:16:50 -0400

    add fireBullet function in EnemyController class

--- src/javascript/EnemyController.js
+++ src/javascript/EnemyController.js
@@ -41,4 +41,14 @@ export default class EnemyController {
         });
         this.enemyRows = this.enemyRows.filter((enemyRow) => enemyRow.length > 0);  
     }
+    fireBullet() {
+        this.fireBulletTimer--;
+        if (this.fireBullet <= 0) {
+            this.fireBulletTimer = this.fireBulletTimerDefault;
+            const allEnemies = this.enemyRows.flat();
+            const enemyIndex = Math.floor(Math.random() * allEnemies.length);
+            const enemy = allEnemies[enemyIndex];
+            this.enemyBulletController.shoot(enemy.x + enemy.width / 2, enemy.y, -3)
+        }
+     }
 }
