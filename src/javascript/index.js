00f68d07183cc0b65d6ded7696b0c56fc58e76be
thaua-arruda <thauafarruda@gmail.com>
2024-05-29 14:19:10 -0400

    add imports

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,5 +1,12 @@
+import EnemyController from "./EnemyController.js";
+import BulletController from "./BulletController.js";
+import Player from "./Player.js";
+    
 const canvas = document.getElementById('game')
 const ctx = canvas.getContext('2d');
 
 canvas.width = 600;
 canvas.height = 600;
+
+const background = new Image();
+background.src = "./src/assets/images/space.png"
