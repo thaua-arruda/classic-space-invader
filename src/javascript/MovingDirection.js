70e1614355c0e27e708daa46c446b390209b1915
thaua-arruda <thauafarruda@gmail.com>
2024-06-03 16:13:16 -0400

    fixed, finish

--- src/javascript/MovingDirection.js
+++ src/javascript/MovingDirection.js
@@ -1,7 +1,7 @@
 const MovingDirection = {
     left: 0,
     right: 1,
-    downRight: 2,
-    downLeft: 3,
-};
-export default MovingDirection;
+    downLeft: 2,
+    downRight: 3,
+  };
+  export default MovingDirection;
